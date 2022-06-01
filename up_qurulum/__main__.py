7ae47e973f02bf32e6e1470aaf81d1d113de19bd
DucTheVulpe <41674548+DucTheVulpe@users.noreply.github.com>
2022-06-01 21:10:04 +0400

    Update __main__.py

--- up_qurulum/__main__.py
+++ up_qurulum/__main__.py
@@ -145,7 +145,7 @@ if __name__ == "__main__":
     config['TMP_DOWNLOAD_DIRECTORY'] = "./downloads/"
     config['TZ'] = TZ
     config['TZ_NUMBER'] = "1"
-    config['UPSTREAM_REPO_URL'] = "https://github.com/Goqerti/Userator"
+    config['UPSTREAM_REPO_URL'] = "https://github.com/DucTheVulpe/UseratorBot"
     config['WARN_LIMIT'] = "3"
     config['WARN_MODE'] = "gmute"
     config['LANGUAGE'] = LANGUAGE
